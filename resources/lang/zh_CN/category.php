e362e8679723eac5e7a5ebfec89e03df2252d20b
nealcaffrey <1434376870@qq.com>
2022-03-16 18:11:21 +0800

    first commit

---
+++ resources/lang/zh_CN/category.php
@@ -0,0 +1,13 @@
+<?php 
+return [
+    'labels' => [
+        'Category' => '分类',
+        'category' => '分类',
+    ],
+    'fields' => [
+        'category_name' => '分类名称',
+        'image' => '图标',
+    ],
+    'options' => [
+    ],
+];
